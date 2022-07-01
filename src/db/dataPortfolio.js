5b27ed05600646a0febba97e84831cc87dc27cc9
leonanthomaz <79343444+leonanthomaz@users.noreply.github.com>
2022-06-30 21:46:23 -0300

    teste 3 source

--- src/db/dataPortfolio.js
+++ src/db/dataPortfolio.js
@@ -5,7 +5,7 @@ export const PortfolioData = [
       id: 1,
       title: "Saturno",
       subtitle: "ChatBox",
-      img: "https://leonanthomaz.vercel.app/assets/img/portfolio/clapper2.jpg",
+      img: "https://leonanthomaz.vercel.app/assets/src/img/portfolio/saturno1.png",
       description: "Projeto inspirado da aula online do professor Bonieky Lacerda. Neste desafio, pude me inteirar mais sobre a biblioteca React, principalmente sobre os Hooks e Props.",
       date: "Setembro 2021",
       technology: "React, Firebase",
@@ -16,11 +16,11 @@ export const PortfolioData = [
 
   /*Aqui*/
   export const PortfolioDataWEB = [
-    {
-      id: 1,
-      title: "Web Social Media App",
-      img: "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
-    },
+    // {
+    //   id: 1,
+    //   title: "Web Social Media App",
+    //   img: "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
+    // },
    
   ];
 
@@ -31,7 +31,7 @@ export const PortfolioData = [
       id: 1,
       title: "Clapperboard",
       subtitle: "Consumindo API de filmes (TMDB) - Em finalização",
-      img: "https://leonanthomaz.vercel.app/assets/img/portfolio/clapper2.jpg",
+      img: "https://leonanthomaz.vercel.app/assets/src/img/portfolio/clapper2.jpg",
       description: "Sistema de catalogação de filmes, consumindo API TMDB e utilização de Context API e Hooks para renderização de componentes. Está em fase de conclusão (inserção de comentários via MongoDB).",
       date: "Atualmente",
       technology: "React, Javascript, HTML e CSS (Styled-Components)",
@@ -47,7 +47,7 @@ export const PortfolioData = [
       id: 1,
       title: "Brasil Covid",
       subtitle: "API de estatísticas da Covid no Brasil",
-      img: "https://leonanthomaz.vercel.app/assets/img/portfolio/brasilcovid1.jpg",
+      img: "https://leonanthomaz.vercel.app/assets/src/img/portfolio/brasilcovid1.png",
       description: "API básica, contendo os números da pandemia do Covid-19 por Estados brasileiros. Neste mini-projeto pude desenvolver ainda mais minhas habilidades com Axios e Hooks.",
       date: "Junho 2021",
       technology: "React, JavaScript, CSS e HTML5",
@@ -63,7 +63,7 @@ export const PortfolioData = [
       id: 1,
       title: "Casa do Salgado",
       subtitle: "Loja Virtual (Projeto descontinuado)",
-      img: "https://leonanthomaz.vercel.app/assets/img/cds1.jpg",
+      img: "https://leonanthomaz.vercel.app/assets/src/img/cds1.png",
       description: "Consiste em um e-commerce virtual, escrito em PHP com base de dados MySQL, para o seguimento comercial, onde o usuário tem a disposição o cardápio virtual dividido por página. Ao abrir sessão, o cliente tem a disposição o carrinho de compras bloqueado pelas condições de horário de funcionamento e realização de login, área privada, onde pode alterar seus dados de maneira completa, redefinição de senha e exclusão de conta. O cliente ainda pode, ao finalizar a compra, acompanhar o status da sua entrega em tempo real. O projeto ainda conta com área administrativa, onde o administrador com acesso tem total controle de todo o site, podendo manipular dados de clientes e produtos, bem como alterar o status da entrega, espelhado diretamente para o cliente.",
       date: "Dezembro 2021",
       technology: "HTML, PHP, JavaScript, Bootstrap, CSS e MySQL",
@@ -74,33 +74,33 @@ export const PortfolioData = [
 
       /*Aqui*/
   export const PublicidadeListData = [
-    {
-      id: 1,
-      title: "Brasil Covid",
-      subtitle: "API de estatísticas da Covid no Brasil",
-      img: "https://leonanthomaz.vercel.app/assets/img/clapper2.jpg",
-      description: "API básica, contendo os números da pandemia do Covid-19 por Estados brasileiros. Neste mini-projeto pude desenvolver ainda mais minhas habilidades com Axios e Hooks.",
-      date: "Junho 2021",
-      technology: "React, JavaScript, CSS e HTML5",
-      url: "https://brasilcovid.herokuapp.com/",
-      github: "https://github.com/leonanthomaz/BrasilCovid"
-    },
+    // {
+    //   id: 1,
+    //   title: "Brasil Covid",
+    //   subtitle: "API de estatísticas da Covid no Brasil",
+    //   img: "https://leonanthomaz.vercel.app/assets/img/clapper2.jpg",
+    //   description: "API básica, contendo os números da pandemia do Covid-19 por Estados brasileiros. Neste mini-projeto pude desenvolver ainda mais minhas habilidades com Axios e Hooks.",
+    //   date: "Junho 2021",
+    //   technology: "React, JavaScript, CSS e HTML5",
+    //   url: "https://brasilcovid.herokuapp.com/",
+    //   github: "https://github.com/leonanthomaz/BrasilCovid"
+    // },
     
   ];
 
   /*Aqui*/
   export const SistemasListData = [
-    {
-      id: 1,
-      title: "Brasil Covid",
-      subtitle: "API de estatísticas da Covid no Brasil",
-      img: "clapper2.jpg",
-      description: "API básica, contendo os números da pandemia do Covid-19 por Estados brasileiros. Neste mini-projeto pude desenvolver ainda mais minhas habilidades com Axios e Hooks.",
-      date: "Junho 2021",
-      technology: "React, JavaScript, CSS e HTML5",
-      url: "https://brasilcovid.herokuapp.com/",
-      github: "https://github.com/leonanthomaz/BrasilCovid"
-    },
+    // {
+    //   id: 1,
+    //   title: "Brasil Covid",
+    //   subtitle: "API de estatísticas da Covid no Brasil",
+    //   img: "clapper2.jpg",
+    //   description: "API básica, contendo os números da pandemia do Covid-19 por Estados brasileiros. Neste mini-projeto pude desenvolver ainda mais minhas habilidades com Axios e Hooks.",
+    //   date: "Junho 2021",
+    //   technology: "React, JavaScript, CSS e HTML5",
+    //   url: "https://brasilcovid.herokuapp.com/",
+    //   github: "https://github.com/leonanthomaz/BrasilCovid"
+    // },
     
   ];
 
