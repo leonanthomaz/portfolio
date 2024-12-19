ad77ec4af78c46afc4b3a7c1d09d96850a75324a
Leonan <79343444+leonanthomaz@users.noreply.github.com>
2024-12-19 00:19:37 -0300

    Update portfolio.ts

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -35,7 +35,7 @@ export const projetosData = {
     },
     {
       id: 2,
-      title: "Análise - Cancer no Brasil",
+      title: "Análise - Câncer no Brasil",
       subtitle: "Análise de Dados",
       description: "Análise interativa de dados sobre diagnósticos de câncer no Brasil (2000-2019), com visualizações sobre distribuição por faixa etária, gênero e frequência de doenças. Baseado em dados do Kaggle.",
       img: pythonImg,
@@ -93,7 +93,7 @@ export const projetosData = {
     },
     {
       id: 2,
-      title: "Análise - Cancer no Brasil",
+      title: "Análise - Câncer no Brasil",
       subtitle: "Análise de Dados",
       description: "Análise interativa de dados sobre diagnósticos de câncer no Brasil (2000-2019), com visualizações sobre distribuição por faixa etária, gênero e frequência de doenças. Baseado em dados do Kaggle.",
       img: pythonImg,
@@ -202,7 +202,7 @@ export const projetosData = {
     },
     {
       id: 6,
-      title: "Análise - Cancer no Brasil",
+      title: "Análise - Câncer no Brasil",
       subtitle: "Análise de Dados",
       description: "Análise interativa de dados sobre diagnósticos de câncer no Brasil (2000-2019), com visualizações sobre distribuição por faixa etária, gênero e frequência de doenças. Baseado em dados do Kaggle.",
       img: pythonImg,
